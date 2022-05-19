5d5dd21d672a85a1fa2bec316b7f63b0a065ab0f
lesept777 <lesept777@gmail.com>
2022-05-19 10:18:53 +0200

    Add files via upload

--- examples/MMLP_BostonHousing/MMLP_BostonHousing.ino
+++ examples/MMLP_BostonHousing/MMLP_BostonHousing.ino
@@ -14,7 +14,6 @@ const char networkFile[] = "/Network_Housing.txt";
 
 // Declare the network
 int Neurons[] = {13, 50, 30, 1};
-int Activations[] = {RELU, RELU, SIGMOID};
 MLP Net(Neurons, 4, 1);
 
 void setup() {
@@ -26,13 +25,13 @@ void setup() {
   }
 
   // Load the dataset from csv file
-  std::vector<std::vector<float> > x;
-  std::vector<std::vector<float> > y;
+  MLMatrix<float> x, y;
   int nData = Net.readCsvFromSpiffs(datasetFile, x, y); // Read dataset
   Net.normalizeDataset(x, y, 1); // Move data in [0, 1] interval
   Net.setTrainTest(4, 1, 1); // Split dataset to training, validation and test
 
   // Set hyperparameters
+  int Activations[] = {RELU, RELU, SIGMOID};
   Net.setActivations (Activations);
   Net.setHyper(0.25f, 0.5f); // LR & momentum
 
@@ -40,14 +39,16 @@ void setup() {
   long heuristics = H_INIT_OPTIM +
                     H_MUTA_WEIGH +
                     H_CHAN_LRLIN +
+                    H_SELE_WEIGH +
+                    H_CHAN_MOMEN +
                     H_CHAN_SGAIN;
   Net.setHeuristics(heuristics);
-//  bool initialize = !Net.netLoad(networkFile);
-//  Net.setHeurInitialize(initialize); // No need to init a new network if we read it from SPIFFS
+  //  bool initialize = !Net.netLoad(networkFile);
+  //  Net.setHeurInitialize(initialize); // No need to init a new network if we read it from SPIFFS
   Net.displayHeuristics(); // Display the heuristics parameters
 
   // Train baby, train...
-  Net.run (x, y, 20, 15, 0.04f);
+  Net.run (x, y, 30, 15, 0.04f);
   Net.netSave(networkFile);
 
   // Prediction
@@ -55,8 +56,8 @@ void setup() {
   MLMatrix<float> u(Neurons[0], 1, 0.0f); // Input array
   for (int i = 0; i < 10; i++) {
     int k = random(nData);
-    for (int j = 0; j < Neurons[0]; ++j) u(j, 0) = x[k][j];
-    float expected = y[k][0];
+    for (int j = 0; j < Neurons[0]; ++j) u(j, 0) = x(k, j);
+    float expected = y(k, 0);
     MLMatrix<float> yhat = Net.predict(u);
     float predicted = yhat(0, 0);
     Serial.printf ("Validation %d: prediction %f, expected %f --> ",
